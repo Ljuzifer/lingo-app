561b53aba9e926a8fbce007e56c85525e3d2ea44
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2024-02-18 20:40:30 +0200

    clean-deploy-build-firebaseConfig

---
+++ src/server/firebaseConfig.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase, ref } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  databaseURL: "https://lingo-db-default-rtdb.firebaseio.com",
+};
+
+export const app = initializeApp(firebaseConfig);
+console.log(process.env);
+
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+
+export const dbRef = ref(db);
